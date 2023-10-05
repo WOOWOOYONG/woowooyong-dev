b89633acfdcabdd307276ebd6145e8f55e6cf758
woowooyong <woowooyong@gmail.com>
2023-10-05 19:52:29 +0800

    update: 更新目錄&筆記

--- components/TableOfContents.vue
+++ components/TableOfContents.vue
@@ -2,20 +2,25 @@
 const props = withDefaults(defineProps<{ activeTocId: string }>(), {})
 
 const router = useRouter()
+const { path } = useRoute()
+const emits = defineEmits(['updateActiveId'])
 
 const sliderHeight = useState('sliderHeight', () => 0)
 const sliderTop = useState('sliderTop', () => 0)
 const tocLinksH2: Ref<Array<HTMLElement>> = ref([])
 const tocLinksH3: Ref<Array<HTMLElement>> = ref([])
 
-const { data: blogPost } = await useAsyncData(`blogToc`, () => queryContent(`/`).findOne())
-const tocLinks = computed(() => blogPost.value?.body.toc.links ?? [])
+const { data: blogPost } = await useAsyncData(`blogToc`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
+const tocLinks = computed(() => blogPost.value?.body.toc!.links ?? [])
 
 const onClick = (id: string) => {
   const el = document.getElementById(id)
   if (el) {
     router.push({ hash: `#${id}` })
     el.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    emits('updateActiveId', id)
   }
 }
 
@@ -38,43 +43,42 @@ watchDebounced(
 </script>
 
 <template>
-  <div class="max-h-82 overflow-auto">
-    <h4>Table of Contents</h4>
-    <nav class="mt-4 flex">
-      <div class="bg-secondary relative w-0.5 overflow-hidden rounded">
-        <div
-          class="absolute left-0 w-full rounded bg-red-500 transition-all duration-200"
-          :style="{ height: `${sliderHeight}px`, top: `${sliderTop}px` }"
-        ></div>
-      </div>
-      <ul class="ml-0 pl-4">
-        <li
-          v-for="{ id, text, children } in tocLinks"
-          :id="`toc-${id}`"
-          :key="id"
-          ref="tocLinksH2"
-          class="mb-2 ml-0 cursor-pointer list-none text-sm last:mb-0"
-          :class="{ 'font-bold': id === activeTocId }"
-          @click="onClick(id)"
-        >
-          {{ text }}
-          <ul v-if="children" class="my-2 ml-3">
-            <li
-              v-for="{ id: childId, text: childText } in children"
-              :id="`toc-${childId}`"
-              :key="childId"
-              ref="tocLinksH3"
-              class="mb-2 ml-0 cursor-pointer list-none text-xs last:mb-0"
-              :class="{ 'font-bold': childId === activeTocId }"
-              @click.stop="onClick(childId)"
-            >
-              {{ childText }}
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <nav class="mt-4 flex">
+    <ul class="ml-0">
+      <li
+        v-for="{ id, text, children } in tocLinks"
+        :id="`toc-${id}`"
+        :key="id"
+        ref="tocLinksH2"
+        class="mb-2 ml-0 cursor-pointer list-none last:mb-0"
+        :class="
+          id === activeTocId
+            ? 'font-bold text-emerald-600 dark:text-emerald-400'
+            : 'dark:text-gray-100'
+        "
+        @click="onClick(id)"
+      >
+        {{ text }}
+        <ul v-if="children" class="my-2 ml-3">
+          <li
+            v-for="{ id: childId, text: childText } in children"
+            :id="`toc-${childId}`"
+            :key="childId"
+            ref="tocLinksH3"
+            class="mb-2 ml-3 cursor-pointer list-none text-sm last:mb-0"
+            :class="
+              childId === activeTocId
+                ? 'font-bold text-emerald-600 dark:text-emerald-400'
+                : 'dark:text-gray-100'
+            "
+            @click.stop="onClick(childId)"
+          >
+            {{ childText }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,76 +2,109 @@
 const { path } = useRoute()
 // const slug = useRoute().params.slug.toString().replace(/,/g, '/')
 
-const { data } = await useAsyncData(`content-${path}`, () => {
+const { data } = (await useAsyncData(`content-${path}`, () => {
   return queryContent().where({ _path: path }).findOne()
-})
+})) as { data: any }
 
-// const { data: navigation } = await useAsyncData('navigation', () => {
-//   return queryContent(path).findOne()
+// 目錄 (old)
+// const currentId = ref('')
+
+// const setActive = (id: string) => {
+//   currentId.value = id
+// }
+
+// type TextNode = {
+//   type: 'text'
+//   value: string
+// }
+
+// type ElementNode = {
+//   type: 'element'
+//   tag: string
+//   props: {
+//     [key: string]: any
+//   }
+//   children: TextNode[]
+// }
+
+// type ContentItem = {
+//   id: string
+//   title: string
+//   depth: number
+// }
+
+// const toc = computed(() => {
+//   if (!data.value) {
+//     return []
+//   }
+//   const items = data.value.body?.children
+//   if (!items) {
+//     return []
+//   }
+
+//   const toc: ContentItem[] = []
+//   const tags = ['h2', 'h3', 'h4', 'h5', 'h6']
+//   items.forEach((item: ElementNode) => {
+//     if (tags.includes(item.tag)) {
+//       toc.push({
+//         id: item.props.id,
+//         title: item.props.id.toString().replace(/-/g, ' '),
+//         depth: Number(item.tag.replace(/h/g, ''))
+//       })
+//     }
+//   })
+//   return toc
 // })
 
-const currentId = ref('')
+// 目錄(new)
+const activeTocId = ref<string>('')
+const nuxtContent = ref(null)
 
-const setActive = (id: string) => {
-  currentId.value = id
-}
-
-// 目錄
-type TextNode = {
-  type: 'text'
-  value: string
-}
-
-type ElementNode = {
-  type: 'element'
-  tag: string
-  props: {
-    [key: string]: any
-  }
-  children: TextNode[]
-}
-
-type ContentItem = {
-  id: string
-  title: string
-  depth: number
-}
-
-const toc = computed(() => {
-  if (!data.value) {
-    return []
-  }
-  const items = data.value.body?.children
-  if (!items) {
-    return []
-  }
-
-  const toc: ContentItem[] = []
-  const tags = ['h2', 'h3', 'h4', 'h5', 'h6']
-  items.forEach((item: ElementNode) => {
-    if (tags.includes(item.tag)) {
-      toc.push({
-        id: item.props.id,
-        title: item.props.id.toString().replace(/-/g, ' '),
-        depth: Number(item.tag.replace(/h/g, ''))
-      })
-    }
-  })
-  return toc
+const observer: Ref<IntersectionObserver | null | undefined> = ref(null)
+const observerOptions = reactive({
+  root: nuxtContent.value,
+  threshold: 1
 })
+
+onMounted(async () => {
+  await nextTick(() => {
+    observer.value = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute('id') as string
+        if (entry.isIntersecting) {
+          activeTocId.value = id
+        }
+      })
+    }, observerOptions)
+
+    document.querySelectorAll('.article h2[id], .article h3[id]').forEach((section) => {
+      observer.value?.observe(section)
+    })
+  })
+})
+
+onUnmounted(() => {
+  observer.value?.disconnect()
+})
+
+const updateId = (newId: string) => {
+  activeTocId.value = newId
+  observer.value?.disconnect()
+}
 </script>
 
 <template>
   <section
     class="h-fit flex-col-reverse items-start justify-center gap-16 pb-24 pt-12 xl:flex xl:flex-row"
   >
-    <div class="grid-cols-12 xl:grid">
-      <article class="col-span-9">
+    <div v-if="data" class="grid-cols-12 xl:grid">
+      <article class="article col-span-9">
         <ContentRenderer
+          ref="nuxtContent"
           :value="data!"
-          class="prose mx-auto dark:prose-invert prose-headings:pt-16 prose-a:no-underline prose-headings:no-underline prose-h1:pt-0 prose-h1:text-center prose-th:border prose-th:border-gray-600 prose-th:p-2 prose-td:border prose-td:border-gray-600 prose-td:p-2 prose-pre:bg-zinc-100 prose-pre:dark:bg-gray-900"
+          class="prose mx-auto dark:prose-invert prose-headings:pt-16 prose-headings:no-underline prose-h1:pt-0 prose-h1:text-center prose-a:border-b-2 prose-a:border-b-gray-700 prose-a:pb-[2px] prose-a:no-underline prose-pre:bg-zinc-100 prose-th:border prose-th:border-gray-600 prose-th:p-2 prose-td:border prose-td:border-gray-600 prose-td:p-2 prose-pre:dark:bg-gray-900"
         />
-        <div v-if="data!.tags.length > 0" class="mt-12 flex justify-center">
+        <div v-if="data.tags.length > 0" class="mt-12 flex justify-center">
           <a
             v-for="tag in data!.tags"
             :key="tag"
@@ -82,7 +115,7 @@ const toc = computed(() => {
           </a>
         </div>
       </article>
-      <nav v-if="data" class="col-span-3 lg:mx-4 lg:px-4 lg:pt-20">
+      <!-- <nav class="col-span-3 lg:mx-4 lg:px-4 lg:pt-20">
         <div
           class="sticky right-0 top-20 order-last hidden border-l border-l-gray-700 px-6 opacity-80 xl:inline-block"
         >
@@ -102,12 +135,19 @@ const toc = computed(() => {
             </li>
           </ul>
         </div>
-      </nav>
+      </nav> -->
+      <div class="col-span-3 lg:mx-4 lg:pt-20">
+        <div
+          class="sticky right-0 top-20 order-last hidden border-l border-l-gray-700 px-6 opacity-80 xl:inline-block"
+        >
+          <h2 class="mb-4 text-xl font-medium dark:text-gray-200">目錄</h2>
+          <TableOfContents :active-toc-id="activeTocId!" @update-active-id="updateId" />
+        </div>
+      </div>
     </div>
-
-    <!-- <div vclass="mx-auto">
+    <div v-else vclass="mx-auto">
       <p>目前還沒有文章</p>
-    </div> -->
+    </div>
   </section>
 </template>
 <style scoped></style>
