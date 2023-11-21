7504e85f0cb74791283fa68040301f382b73c4fc
woowooyong <woowooyong@gmail.com>
2023-11-21 23:27:59 +0800

    chore: 刪除多餘程式碼

--- pages/categories/[category].vue
+++ pages/categories/[category].vue
@@ -6,16 +6,7 @@ definePageMeta({
   key: (route) => route.fullPath
 })
 
-// const category = computed(() => {
-//   return route.params.category
-// })
 const category = route.params.category as string
-
-// const { data: notes } = await useAsyncData(`category-${category.value}`, () =>
-//   queryContent('/notes')
-//     .where({ category: { $contains: category.value } })
-//     .find()
-// )
 </script>
 
 <template>
@@ -26,36 +17,6 @@ const category = route.params.category as string
         <CategoryTag />
       </aside>
       <div class="sm:col-span-7 xl:pr-52">
-        <!-- <ul v-if="notes!.length > 0">
-          <li
-            v-for="(note, index) in notes"
-            :key="index"
-            class="my-8 border-b border-slate-600 py-4 dark:text-gray-200"
-          >
-            <NuxtLink
-              :to="note._path"
-              class="block text-2xl font-bold duration-300 hover:-translate-x-1"
-            >
-              {{ note.title }}
-            </NuxtLink>
-            <p class="my-2 line-clamp-1 opacity-70">
-              {{ note.description }}
-            </p>
-            <div class="mb-4 flex items-center gap-2">
-              <span class="pb-1"><Icon name="material-symbols:calendar-today-rounded" /></span>
-              <span class="">{{ note.date }}</span>
-            </div>
-
-            <div class="flex gap-4">
-              <span
-                v-for="(tag, index) in note.tags"
-                :key="index"
-                class="inline-flex items-center justify-center rounded border border-blue-300 bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 hover:bg-blue-200 dark:bg-gray-700 dark:text-blue-400"
-                ># {{ tag }}</span
-              >
-            </div>
-          </li>
-        </ul> -->
         <ClientOnly>
           <ContentList
             path="/notes"
