3fafbfbab9605c076118002f104909872666972c
woowooyong <woowooyong@gmail.com>
2023-12-15 15:13:38 +0800

    feat: 新增顯示筆記數量

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@ useSeoMeta({
   description: '歡迎來到我的開發基地',
   ogDescription: '歡迎來到我的開發基地'
 })
-// const count = await queryContent('notes').count()
 </script>
 
 <template>
@@ -13,7 +12,6 @@ useSeoMeta({
     <div class="px-10 py-6">
       <AboutMe />
     </div>
-    <!-- <h3>目前共有 {{ count }} 篇筆記</h3> -->
   </div>
 </template>
 
