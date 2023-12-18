97345a03610bb87c8e0c8083bd26c2b5725d05b1
woowooyong <woowooyong@gmail.com>
2023-12-18 10:17:23 +0800

    update: 修改首頁樣式

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -15,37 +15,43 @@ const cardTransform = computed(() => {
 
 <template>
   <div>
-    <section class="container flex flex-col items-center justify-center gap-10 pt-10">
+    <section class="container flex flex-col items-center justify-center gap-3 pt-4">
       <h2 class="my-2 text-center text-2xl font-medium dark:text-gray-200">歡迎來到我的基地</h2>
       <div
-        ref="myCard"
+        class="card-shadhow overflow-hidden rounded-md border-8 border-zinc-300 bg-zinc-300 dark:border-stone-400 dark:bg-stone-400"
         :style="{ transform: cardTransform }"
-        class="personal-card rounded-md border-8 border-zinc-300 bg-slate-100 p-6 dark:border-stone-400 dark:bg-slate-700 dark:text-gray-200"
       >
-        <div class="mb-10 flex flex-col items-center gap-4">
-          <ul class="flex w-full items-center justify-end gap-2">
-            <li><NuxtImg format="webp" src="/icons/tailwind.svg" alt="vue-icon" class="w-10" /></li>
-            <li><NuxtImg format="webp" src="/icons/nuxt.svg" alt="vue-icon" class="w-10" /></li>
-            <li><NuxtImg format="webp" src="/icons/vue.svg" alt="vue-icon" class="w-8" /></li>
-          </ul>
-          <div>
-            <NuxtImg format="webp" src="/Me.jpg" alt="avatar" class="h-40 w-40 rounded-full" />
-          </div>
-          <div>
-            <h1 class="mb-2 text-center text-xl font-medium">YONG</h1>
-            <div class="flex items-center justify-center gap-4">
-              <p class="font-semibold text-gray-600 dark:text-gray-200">Front-End Developer</p>
+        <div
+          ref="myCard"
+          class="personal-card rounded-md bg-slate-100 p-6 dark:bg-slate-700 dark:text-gray-200"
+        >
+          <div class="mb-10 flex flex-col items-center gap-4">
+            <ul class="flex w-full items-center justify-end gap-2">
+              <li>
+                <NuxtImg format="webp" src="/icons/tailwind.svg" alt="vue-icon" class="w-10" />
+              </li>
+              <li><NuxtImg format="webp" src="/icons/nuxt.svg" alt="vue-icon" class="w-10" /></li>
+              <li><NuxtImg format="webp" src="/icons/vue.svg" alt="vue-icon" class="w-8" /></li>
+            </ul>
+            <div>
+              <NuxtImg format="webp" src="/Me.jpg" alt="avatar" class="h-40 w-40 rounded-full" />
+            </div>
+            <div>
+              <h1 class="mb-2 text-center text-xl font-medium">YONG</h1>
+              <div class="flex items-center justify-center gap-4">
+                <p class="font-semibold text-gray-600 dark:text-gray-200">Front-End Developer</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="text-left text-lg">
-          <p class="mb-2">關於我：</p>
-          <p class="mb-4 leading-8">
-            嚮往DIY的生活方式．樂於研究及探索新技術<br />
-            喜歡動手實作，注重細節，享受累積經驗值升等的過程<br />
-            以成為前端工程師為目標努力中......
-          </p>
-          <p class="">目前主要開發工具：Vue生態系</p>
+          <div class="text-left text-lg">
+            <p class="mb-2">關於我：</p>
+            <p class="mb-4 leading-8">
+              嚮往DIY的生活方式．樂於研究及探索新技術<br />
+              喜歡動手實作，注重細節，享受累積經驗值升等的過程<br />
+              以成為前端工程師為目標努力中......
+            </p>
+            <p class="">目前主要開發工具：Vue生態系</p>
+          </div>
         </div>
       </div>
     </section>
@@ -53,11 +59,9 @@ const cardTransform = computed(() => {
 </template>
 
 <style scoped>
-.personal-card {
+.card-shadhow {
   box-shadow: 5px 20px 30px rgba(0, 0, 0, 0.2);
   transition: transform 0.5s ease;
+  background-clip: padding-box;
 }
-/* .container {
-  perspective: 1000px;
-} */
 </style>
