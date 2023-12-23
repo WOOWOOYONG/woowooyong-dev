580c70bad6f031535d7eafd32902cd13ed919fa7
woowooyong <woowooyong@gmail.com>
2023-12-23 14:49:04 +0800

    update: 筆記Tag改成元件

---
+++ components/NoteTag.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{ noteTags: string[] }>()
+</script>
+
+<template>
+  <ul v-if="noteTags.length > 0" class="flex gap-4">
+    <li
+      v-for="(tag, index) in noteTags"
+      :key="index"
+      class="inline-flex items-center justify-center rounded border border-blue-300 bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 hover:bg-blue-200 dark:bg-gray-700 dark:text-blue-400"
+    >
+      # {{ tag }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
--- pages/categories/[category].vue
+++ pages/categories/[category].vue
@@ -53,15 +53,7 @@ const category = route.params.category
                       /></span>
                       <span class="">{{ note.date }}</span>
                     </div>
-                    <ul class="flex gap-4">
-                      <li
-                        v-for="(tag, n) in note.tags"
-                        :key="n"
-                        class="inline-flex items-center justify-center rounded border border-blue-300 bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 hover:bg-blue-200 dark:bg-gray-700 dark:text-blue-400"
-                      >
-                        {{ tag }}
-                      </li>
-                    </ul>
+                    <NoteTag :note-tags="note.tags" />
                   </div>
                 </li>
               </ul>
